7c67ac137a138dba03cf0bdb3d16a76f7d64483d
Sharp <rsha7868@740s-pl-127.local>
2018-05-16 13:46:09 -0600

    Trying to start a clock onto the project.

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,9 +1,25 @@
 package controller;
+import model.Pannel;
 
+import java.sql.Time;
+
+import model.Frame;
 public class Controller
 {
-public void start()
-{
-	
-}
+	public void start()
+	{
+		int time = 0;
+		{
+			while(time == 0)
+			{
+				time++;
+				if(time == 60)
+				{
+				System.out.println("At 60");
+					
+				
+				}
+			}
+		}
+	}
 }
--- src/model/Pannel.java
+++ src/model/Pannel.java
@@ -2,5 +2,5 @@ package model;
 
 public class Pannel
 {
-
+	time = new Clocktime();
 }
