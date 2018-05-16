dd9d51296d79092e9031f3f47e021cdf8e26b40a
Sharp <rsha7868@740s-pl-127.local>
2018-05-16 13:50:43 -0600

    SAving the making of the clock.

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -8,16 +8,14 @@ public class Controller
 {
 	public void start()
 	{
-		int time = 0;
+		int clockTime = 0;
 		{
-			while(time == 0)
+			while(clockTime == 0)
 			{
-				time++;
-				if(time == 60)
+				clockTime++;
+				if(clockTime == 60)
 				{
 				System.out.println("At 60");
-					
-				
 				}
 			}
 		}
--- src/model/Pannel.java
+++ src/model/Pannel.java
@@ -1,6 +1,6 @@
 package model;
-
+import java.
 public class Pannel
 {
-	time = new Clocktime();
+	int clockTime;
 }
